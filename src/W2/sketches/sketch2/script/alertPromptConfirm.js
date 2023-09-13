2625a29485c36f30ce434c818d08a003af04a477
leejigu <138595306+leejigu@users.noreply.github.com>
2023-09-14 00:12:01 +0900

    강의 완강!! 근데이제 과제가 남은

--- src/W2/sketches/sketch2/script/alertPromptConfirm.js
+++ src/W2/sketches/sketch2/script/alertPromptConfirm.js
@@ -1,3 +1,11 @@
-alert('나의 홈페이지의 당도한 것을 환영하오 낯선이여');
-let userTyped = prompt('자네 이름이 무엇인가?', '홍길동');
-confirm('그러니까 너의 이름이? ' + userTyped + '이라는 거지?');
+// alert('나의 홈페이지의 당도한 것을 환영하오 낯선이여');
+// let userTyped = prompt('자네 이름이 무엇인가?', '홍길동');
+// confirm('그러니까 너의 이름이? ' + userTyped + '이라는 거지?');
+
+alert('안녕하세요');
+let aaaa;
+aaaa = prompt('당신의 이름은?', '쥬쥬대격돌');
+let confirmval = confirm('당신의 이름이 ' + aaaa + '이/가 맞습니까?');
+if (confirmval == true) {
+  alert('환영합니다. ' + aaaa + '님');
+}
---
+++ src/W2/sketches/sketch7/script/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  setCanvasContainer('canvas-goes-hear', 3, 2, true);
+  background('#ff44e3');
+}
+
+function draw() {
+  background('#ff44e3');
+  circle(mouseX, mouseY, width * 0.1);
+}
