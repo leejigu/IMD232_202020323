466ca03e2b8db429f12e96c394b6cc0b02e7cec6
leejigu <138595306+leejigu@users.noreply.github.com>
2023-11-21 17:28:31 +0900

    10w

--- src/W10/sketches/sketch1/script/sketch.js
+++ src/W10/sketches/sketch1/script/sketch.js
@@ -1,25 +1,24 @@
-let aVariable = 20;
-let anArray = [30, 60, 90];
-let anotherArray = [];
+let cam;
 
 function setup() {
   setCanvasContainer('canvas', 3, 2, true);
-  console.log('aVariable', aVariable);
-  console.log('anArray', anArray);
-  console.log('anArray[0]', anArray[0]);
-  console.log('anArray[1]', anArray[1]);
-  console.log('anArray[2]', anArray[2]);
-  console.log('anArray.length', anArray.length);
-  console.log('anotherArray', anotherArray);
-  console.log('anotherArray.length', anotherArray.length);
-  console.log('anotherArray[0]', anotherArray[0]);
-  console.log('anotherArray[1]', anotherArray[1]);
-  anotherArray.push('어레이에 넣은 첫 데이터');
-  console.log('anotherArray[0]', anotherArray[0]);
-  anotherArray.push(50);
-  console.log('anotherArray[1]', anotherArray[1]);
-
-  background(255);
+  cam = createCapture(VIDEO);
+  cam.hide();
+  console.log(cam);
+  noLoop();
 }
-function draw() {}
-JavaScriptDownload;
+function draw() {
+  background ('white');
+  image (cam, 0, 0, width, (cam.height / cam.width) * width
+  cam. loadPixels ();
+  console. log('width', cam.width);
+  console. log('height', cam. height); 
+  console. log('pixel', cam.pixels[0];
+  for (let y = 0; y < cam.height; y++) {
+    for (let x = 0; x < cam.width; x++) {
+      const idx = cam width * y + x;
+      const brightmess = brifhtness[color];
+      ellipse(x,y,brifhtness / 255 * 20);
+      
+    }
+  }
