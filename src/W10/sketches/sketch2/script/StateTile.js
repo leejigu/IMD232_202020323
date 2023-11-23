e77912c56a68b042e39ea74560dc8d25ee48d483
leejigu <138595306+leejigu@users.noreply.github.com>
2023-11-23 17:00:14 +0900

    A

--- src/W10/sketches/sketch2/script/StateTile.js
+++ src/W10/sketches/sketch2/script/StateTile.js
@@ -5,29 +5,56 @@ let tileW, tileH;
 
 const tiles = [];
 
+let run = false;
+
 function setup() {
   setCanvasContainer('canvas', 1, 1, true);
 
   tileW = width / tileCol;
   tileH = height / tileRow;
 
-  randomSeed(1);
+  randomSeed(4);
   for (let row = 0; row < tileRow; row++) {
     for (let col = 0; col < tileCol; col++) {
       const tileX = tileW * col;
       const tileY = tileH * row;
-      const newTile = new StateTile(tileX, tileY, tileW, tileH);
-      if (random() > 0.5) newTile.state = false;
+      const idx = tileCol * row + col;
+      const newTile = new StateTile(tileX, tileY, tileW, tileH, idx);
+      // if (random() > 0.5) newTile.state = false;
+      newTile.state = false;
       tiles.push(newTile);
     }
   }
+
+  tiles.forEach((eachTile) => {
+    eachTile.setNeighbors(tiles);
+  });
+
+  frameRate(15);
+  background('white');
+  tiles.forEach((eachTile) => {
+    eachTile.display(mouseX, mouseY);
+  });
 }
 function draw() {
   background('white');
 
+  if (run) {
+    tiles.forEach((eachTile) => {
+      eachTile.calcNextState();
+    });
+    tiles.forEach((eachTile) => {
+      eachTile.update();
+    });
+  }
+
   tiles.forEach((eachTile) => {
     eachTile.display(mouseX, mouseY);
   });
+}
+
+function keyPressed() {
+  run = !run;
 }
 
 function mouseClicked() {
--- src/W10/sketches/sketch2/script/sketch.js
+++ src/W10/sketches/sketch2/script/sketch.js
@@ -1,31 +1,105 @@
-let cells = [];
+class StateTile {
+  constructor(x, y, w, h, idx) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.state = true;
+    this.nextState;
+    this.idx = idx;
+    this.neighbors = [];
+  }
 
-const colNum = 10,
-  rowNum = colNum;
+  setNeighbors(tiles) {
+    const neighborsIdx = [
+      this.idx - tileCol - 1,
+      this.idx - tileCol,
+      this.idx - tileCol + 1,
+      this.idx + 1,
+      this.idx + tileCol + 1,
+      this.idx + tileCol,
+      this.idx + tileCol - 1,
+      this.idx - 1,
+    ];
+    const myCol = this.idx % tileCol;
+    const myRow = floor(this.idx / tileCol);
+    if (myCol === 0) {
+      neighborsIdx[0] = -1;
+      neighborsIdx[7] = -1;
+      neighborsIdx[6] = -1;
+    } else if (myCol === tileCol - 1) {
+      neighborsIdx[2] = -1;
+      neighborsIdx[3] = -1;
+      neighborsIdx[4] = -1;
+    }
+    if (myRow === 0) {
+      neighborsIdx[0] = -1;
+      neighborsIdx[1] = -1;
+      neighborsIdx[2] = -1;
+    } else if (myRow === tileRow - 1) {
+      neighborsIdx[6] = -1;
+      neighborsIdx[5] = -1;
+      neighborsIdx[4] = -1;
+    }
 
-let w, h;
+    neighborsIdx.forEach((eachIdx) => {
+      this.neighbors.push(eachIdx > -1 ? tiles[eachIdx] : null);
+    });
+  }
 
-function setup() {
-  setCanvasContainer('canvas', 1, 1, true);
-
-  w = width / colNum;
-  h = height / rowNum;
-
-  for (let row = 0; row < rowNum; row++) {
-    for (let col = 0; col < colNum; col++) {
-      const x = w * col;
-      const y = h * row;
-      let state = random() < 0.5;
-      cells.push(new Cell(x, y, w, h, state));
+  calcNextState() {
+    let sum = 0;
+    this.neighbors.forEach((eachNeighbor) => {
+      if (eachNeighbor !== null) {
+        if (eachNeighbor.state) sum++;
+      }
+    });
+    if (this.state) {
+      if (sum < 2 || sum > 3) {
+        this.nextState = false;
+      } else {
+        this.nextState = this.state;
+      }
+    } else {
+      if (sum === 3) {
+        this.nextState = true;
+      } else {
+        this.nextState = this.state;
+      }
     }
   }
 
-  background('white');
-}
+  update() {
+    this.state = this.nextState;
+  }
 
-function draw() {
-  background('white');
-  cells.forEach((eachCell) => {
-    eachCell.display();
-  });
+  isHover(mx, my) {
+    return (
+      mx >= this.x &&
+      mx < this.x + this.w &&
+      my >= this.y &&
+      my < this.y + this.h
+    );
+  }
+
+  changeState(mx, my) {
+    if (!this.isHover(mx, my)) return false;
+    this.state = !this.state;
+    // console.log('this.idx', this.idx);
+    console.log('this.neighbors', this.neighbors);
+    return true;
+  }
+
+  display(mx, my) {
+    push();
+    translate(this.x, this.y);
+
+    fill(this.state ? 64 : 255);
+
+    strokeWeight(this.isHover(mx, my) ? 4 : 1);
+    stroke(this.isHover(mx, my) ? 'red' : 'black');
+
+    rect(0, 0, this.w, this.h);
+    pop();
+  }
 }
