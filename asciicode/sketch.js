592729c18dc871f2bf8cbd9191f11ce34ec70a05
leejigu <138595306+leejigu@users.noreply.github.com>
2023-12-17 05:35:14 +0900

    윈도우배경바꾸기 전

--- asciicode/sketch.js
+++ asciicode/sketch.js
@@ -1,20 +1,31 @@
-const density = '@%#*+=-:. ';
+const density = '*@%#:.  ';
 let video;
 let asciiDiv;
 
 function setup() {
+  dom = select('#canvas');
+  //   console.log('p5 select', dom);
+  //   console.log('p5 select', dom.width);
+
+  htmlDom = document.querySelector('#canvas');
+  //   console.log('querySelector', htmlDom);
   noCanvas();
   video = createCapture(VIDEO, function () {
     video.elt.setAttribute('playsinline', ''); // iOS에서 영상이 인라인으로 재생되도록 설정
-    video.size(140, 110); // 카메라의 종횡비에 맞게 크기 조절
+    video.size(120, 95); // 카메라의 종횡비에 맞게 크기 조절
   });
 
   asciiDiv = createDiv();
   asciiDiv.style('font-family', 'monospace'); // 정렬을 위해 고정폭 글꼴 설정
   asciiDiv.style('white-space', 'pre'); // 공백 유지
-  asciiDiv.style('font-size', '12px'); // 글꼴 크기 조절 (16px로 변경)
+  asciiDiv.style('font-size', '16px'); // 글꼴 크기 조절 (16px로 변경)
   asciiDiv.style('background-color', 'black'); // 검정 배경 설정
-  asciiDiv.style('color', 'white'); // 흰색 폰트 설정
+  asciiDiv.style('color', '#ff00ff'); // 흰색 폰트 설정
+  // 추가: 아스키 아트에 흰색 여백 추가
+  asciiDiv.style('margin', '4px');
+  asciiDiv.style('border', '6px solid #e8e8d8');
+  asciiDiv.style('border-bottom', '46px solid #e8e8d8'); // 하단의 border를 12px로 설정
+  asciiDiv.style('padding', '2px '); // 테두리 안쪽에 여백 추가
 }
 
 function draw() {
